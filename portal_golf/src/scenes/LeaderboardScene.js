5d1a501c6a090a9e0c5e9a44482842b5f35cc47f
ericmckenzie <eric.c.mckenzie@gmail.com>
2025-03-15 10:39:48 -0400

    starting leaderboard development

---
+++ portal_golf/src/scenes/LeaderboardScene.js
@@ -0,0 +1,68 @@
+import MenuListener from '../handlers/MenuListener.js';
+
+class InstructionsMenuScene extends Phaser.Scene {
+    constructor(){
+        super({ key: 'Leaderboard' });
+    }
+
+    preload(){
+		this.load.json("leaderboard", "https://script.google.com/macros/s/AKfycbyKbsjUJPBh7ij0-qClJwQDGGj2DeQNE3RDQv-pPZ6l26CpYsQS2A6Yz_emv2hmL_lTVg/exec?apiKey=AIzaSyCATNJGxIUKlt01Ps5BKsGPi6BqbTWF3TE");
+    }
+
+    async create(){
+		//Set variables for item placement in scene
+		const swidth = this.scale.width;
+		const sheight = this.scale.height;
+		const borderwidth = 5;
+
+		//Add background image
+		this.add.image(swidth/2, sheight/2, 'instructionsbg');
+
+		//Leaderboard text
+		const textx = swidth/2;
+		const texty = sheight/2;
+		const textStyle = {
+			fontSize: '22px', 
+			fill: '#ffffff', 
+			align: 'center',
+			wordWrap: { width: bwidth - borderwidth }
+		};
+
+		//Get text
+		this.add.text(swidth/2, 100, "Leaderboard", textStyle);
+
+        let leaderboardText = this.add.text(textx, texty, "Loading...", textStyle);
+
+        try {
+            let response = await fetch("https://script.google.com/macros/s/AKfycbyKbsjUJPBh7ij0-qClJwQDGGj2DeQNE3RDQv-pPZ6l26CpYsQS2A6Yz_emv2hmL_lTVg/exec?apiKey=AIzaSyCATNJGxIUKlt01Ps5BKsGPi6BqbTWF3TE");
+            let scores = await response.json();
+            
+            let leaderboardString = scores
+                .slice(0, 10) // Show top 10
+                .map((entry, index) => `${index + 1}. ${entry[0]} - ${entry[1]}`)
+                .join("\n");
+
+            leaderboardText.setText(leaderboardString);
+        } catch (error) {
+            console.error("Error loading leaderboard:", error);
+            leaderboardText.setText("Failed to load leaderboard.");
+        }
+		
+		
+		//Back button
+		const backx = swidth/2;
+		const backy = bgy + bheight * 0.8;
+		const backButton = this.add.image(backx, backy, 'backButton').setInteractive();
+		backButton.on('pointerdown', this.back, this);
+
+		const listener = new MenuListener(this);
+    }
+
+	back(){
+		this.scene.resume('MenuScene');
+		this.scene.stop('Leaderboard');
+	}
+
+}
+
+export default InstructionsMenuScene;
--- portal_golf/src/scenes/MenuScene.js
+++ portal_golf/src/scenes/MenuScene.js
@@ -10,15 +10,18 @@ class MenuScene extends Phaser.Scene {
 		this.load.image('menuScreen', ASSET_FILEPATH_MENU + 'start_bg.png');
         this.load.image('startButton', ASSET_FILEPATH_MENU + 'start_button.png');
         this.load.image('instructionsButton', ASSET_FILEPATH_MENU + 'instructions_button.png');
+        this.load.image('leaderboardButton', ASSET_FILEPATH_MENU + 'menu_leaderboard_button.png');
     }
 
     create() {
         this.add.image(this.scale.width/2,this.scale.height/2,'menuScreen');
         const startButton = this.add.image(this.scale.width/2, 437, 'startButton').setInteractive();
-		const instructionsButton = this.add.image(this.scale.width/2, 537, 'instructionsButton').setInteractive();
+		const instructionsButton = this.add.image(this.scale.width/2 - 150, 537, 'instructionsButton').setInteractive();
+		const leaderboardButton = this.add.image(this.scale.width/2 + 150, 537, 'leaderboardButton').setInteractive();
 
         startButton.on('pointerdown', this.startGame, this);
 		instructionsButton.on('pointerdown', this.instructions, this);
+		leaderboardButton.on('pointerdown', this.leaderBoard, this);
 
 		const listener = new MenuListener(this);
     }
@@ -30,6 +33,11 @@ class MenuScene extends Phaser.Scene {
 
 	instructions(){
 		this.scene.launch('InstructionsMenu');
+        this.scene.pause('MenuScene');
+	}
+
+	leaderBoard(){
+		this.scene.launch('Leaderboard');
         this.scene.pause('MenuScene');
 	}
 
