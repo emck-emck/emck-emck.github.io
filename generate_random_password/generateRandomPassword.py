45775da562b7cfb1e6ebd5b5bb0d3b4ac0ab4f8f
Eric McKenzie <eric.c.mckenzie@gmail.com>
2026-09-02 12:43:40 -0400

    new project added

---
+++ generate_random_password/generateRandomPassword.py
@@ -0,0 +1,21 @@
+import random
+from pyscript import web, when
+
+# Global variables for valid characters
+validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+validLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+validNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+validSpecial = ["!", "@", "#", "$", "%", "^", "&", "*"]
+validChars = [validUpper, validLower, validNumber, validSpecial]
+
+# The generating code
+@when("click", "#genpass")
+def generatePassword(event):
+  n = int(web.page["numchars"])
+  ret = ""
+  for i in range(0, n):
+    rando1 = int(random.random() * 4)
+    rando2 = int(random.random() * len(validChars[rando1]))
+    ret = ret + validChars[rando1][rando2]
+  output = web.page["passoutput"]
+  output.innerText = ret
