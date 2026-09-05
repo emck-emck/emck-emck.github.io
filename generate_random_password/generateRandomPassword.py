c1a2a2769bfab053fc82f59b3c49ef5d23cf80c5
Eric McKenzie <eric.c.mckenzie@gmail.com>
2026-09-05 14:17:28 -0400

    trying something new

--- generate_random_password/generateRandomPassword.py
+++ generate_random_password/generateRandomPassword.py
@@ -1,5 +1,5 @@
 import random
-from pyscript import web
+from pyscript import web, when
 
 # Global variables for valid characters
 validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -12,26 +12,26 @@ validChars = [validUpper, validLower, validNumber, validSpecial]
 def generatePassword(event = None):
   if event:
     event.preventDefault()
-  else:
-    return
 
-  n = int(web.page["numchars"].value)
-  ret = ""
+    # Web input
+    n = int(event.target.value)
 
-  # input santiy
-  if n < 8:
-    return
-
-  # input santiy
-  if n > 24:
-    return
-
-  # The generating code
-  for i in range(0, n):
-    rando1 = int(random.random() * 4)
-    rando2 = int(random.random() * len(validChars[rando1]))
-    ret = ret + validChars[rando1][rando2]
-  output = web.page["passoutput"]
-  output.innerText = ret
+    # Input sanity
+    if n < 8:
+      return
+   
+    # Input sanity
+    if n > 24:
+      return
+   
+    # The generating code
+    ret = ""
+    for i in range(0, n):
+      rando1 = int(random.random() * 4)
+      rando2 = int(random.random() * len(validChars[rando1]))
+      ret = ret + validChars[rando1][rando2]
+   
+    # Web output
+    display(ret, target="passoutput")
 
 Element("inputform").element.onsubmit = generatePassword
